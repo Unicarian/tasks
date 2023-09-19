7041f774214e22f0414bd9cdbc3829376d1ed2f9
Jonathan Malcolm <jonathan.malcolm@maine.edu>
2023-09-18 22:26:33 -0400

    function makeMath completed

--- src/arrays.ts
+++ src/arrays.ts
@@ -93,7 +93,15 @@ export function allRGB(colors: string[]): boolean {
  * And the array [] would become "0=0".
  */
 export function makeMath(addends: number[]): string {
-    return "";
+    if (addends.length === 0) {
+        return "0=0";
+    }
+    const sum = addends.reduce(
+        (currentTotal: number, num: number) => currentTotal + num,
+        0
+    );
+    const output = addends.join("+");
+    return String(sum) + "=" + output;
 }
 
 /**
