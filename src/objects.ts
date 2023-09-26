8994a958832588bfc40102ac5a80070a034dce8d
Jonathan Malcolm <jonathan.malcolm@maine.edu>
2023-09-25 21:41:45 -0400

    function publishQuestion completed

--- src/objects.ts
+++ src/objects.ts
@@ -108,7 +108,11 @@ export function renameQuestion(question: Question, newName: string): Question {
  * published; if it was published, now it should be not published.
  */
 export function publishQuestion(question: Question): Question {
-    return question;
+    if (question.published === false){
+        return { ...question, published: true };
+    } else {
+        return { ...question, published: false };
+    }
 }
 
 /**
