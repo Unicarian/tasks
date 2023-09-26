9346ba42faac0c6319878e27daf07e8cbd52e0c4
Jonathan Malcolm <jonathan.malcolm@maine.edu>
2023-09-25 21:11:21 -0400

    function toShortForm completed

--- src/objects.ts
+++ src/objects.ts
@@ -62,7 +62,8 @@ export function isValid(question: Question, answer: string): boolean {
  * name "My First Question" would become "9: My First Q".
  */
 export function toShortForm(question: Question): string {
-    return "";
+    const { id, name } = question;
+    return id.toString() + ": " + name.slice(0, 10);
 }
 
 /**
