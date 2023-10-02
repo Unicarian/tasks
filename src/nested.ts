ac7ee511ec2270fde6e1d8c96ad82294f0b94e45
Jonathan Malcolm <jonathan.malcolm@maine.edu>
2023-10-02 18:11:50 -0400

    Function sumPoints completed

--- src/nested.ts
+++ src/nested.ts
@@ -72,7 +72,12 @@ export function getNames(questions: Question[]): string[] {
  * Consumes an array of questions and returns the sum total of all their points added together.
  */
 export function sumPoints(questions: Question[]): number {
-    return 0;
+    const points = questions.reduce(
+        (currentSum: number, questions: Question): number =>
+            currentSum + questions.points,
+        0
+    );
+    return points;
 }
 
 /***
