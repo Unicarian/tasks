d75c03dfa16d46bcce29a38bf62ba96701eda17e
Jonathan Malcolm <jonathan.malcolm@maine.edu>
2023-10-16 16:45:41 -0400

    CycleHoliday component complete

--- src/components/CycleHoliday.tsx
+++ src/components/CycleHoliday.tsx
@@ -1,6 +1,61 @@
+/* eslint-disable prettier/prettier */
 import React, { useState } from "react";
 import { Button } from "react-bootstrap";
 
 export function CycleHoliday(): JSX.Element {
-    return <div>Cycle Holiday</div>;
+    const [holiday, setHoliday] = useState<string>("Halloween");
+    const HOLIDAY_EMOJIS: Record<string, string> = {
+        "Halloween": "🎃",
+        "Christmas": "🎄",
+        "New Year's Day": "🎂",
+        "Thanksgiving": "🦃",
+        "St. Patrick's Day": "☘️"
+    };
+
+    function advByDate(): void {
+        switch (holiday) {
+        case "New Year's Day":
+            setHoliday("St. Patrick's Day");
+            break;
+        case "St. Patrick's Day":
+            setHoliday("Halloween");
+            break;
+        case "Halloween":
+            setHoliday("Thanksgiving");
+            break;
+        case "Thanksgiving":
+            setHoliday("Christmas");
+            break;
+        case "Christmas":
+            setHoliday("New Year's Day");
+            break;
+        }
+    }
+
+    function advByName(): void {
+        switch(holiday) {
+        case "Christmas":
+            setHoliday("Halloween");
+            break;
+        case "Halloween":
+            setHoliday("New Year's Day");
+            break;
+        case "New Year's Day":
+            setHoliday("St. Patrick's Day");
+            break;
+        case "St. Patrick's Day":
+            setHoliday("Thanksgiving");
+            break;
+        case "Thanksgiving":
+            setHoliday("Christmas");
+            break;
+        }
+    }
+    return (
+        <div>
+            Holiday: {HOLIDAY_EMOJIS[holiday]}
+            <Button onClick={advByName}>Advance by Alphabet</Button>
+            <Button onClick={advByDate}>Advance by Year</Button>
+        </div>
+    );
 }
