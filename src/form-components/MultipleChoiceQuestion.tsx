83eae6fd3012622308ff69d4d3b445521e345ba7
Jonathan Malcolm <jonathan.malcolm@maine.edu>
2023-11-02 22:13:27 -0400

    MultipleChoiceQuestion completed

--- src/form-components/MultipleChoiceQuestion.tsx
+++ src/form-components/MultipleChoiceQuestion.tsx
@@ -1,4 +1,6 @@
+import exp from "constants";
 import React, { useState } from "react";
+import { Form } from "react-bootstrap";
 
 export function MultipleChoiceQuestion({
     options,
@@ -7,9 +9,27 @@ export function MultipleChoiceQuestion({
     options: string[];
     expectedAnswer: string;
 }): JSX.Element {
+    const [guess, setGuess] = useState<string>(options[0]);
+
+    function updateGuess(e: React.ChangeEvent<HTMLSelectElement>) {
+        setGuess(e.target.value);
+    }
+
     return (
         <div>
             <h3>Multiple Choice Question</h3>
+            <Form.Group controlId="options">
+                <Form.Label></Form.Label>
+                <Form.Select value={guess} onChange={updateGuess}>
+                    {options.map((option: string) => (
+                        <option key={option} value={option}>
+                            {option}
+                        </option>
+                    ))}
+                </Form.Select>
+            </Form.Group>
+            {guess === expectedAnswer && <div>✔️</div>}
+            {guess !== expectedAnswer && <div>❌</div>}
         </div>
     );
 }
